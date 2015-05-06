c81221f7c5beee2e29b5d4c7dc0197dba982a525
Brett Harmon <bharmon@ycp.edu>
2015-05-06 08:19:31 -0400

    Signed-off-by: Brett Harmon <bharmon@ycp.edu>

--- MyServlet/src/net/codejava/getDataAjax.java
+++ MyServlet/src/net/codejava/getDataAjax.java
@@ -1,51 +1,71 @@
 package net.codejava;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class getDataAjax
- * responsible for getting XML data 
- */
-public class getDataAjax extends HttpServlet {
+public class getDataAjax extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
+
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ajaxRequest(request,response);
 	}
 	
-	
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest, HttpServletResponse)
-	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ajaxRequest(request,response);
 	}
 	
-	private void ajaxRequest(HttpServletRequest req,HttpServletResponse resp) {
-		System.out.println("Data requested");
+	private void ajaxRequest(HttpServletRequest req,HttpServletResponse resp) throws IOException {
+		
+		System.out.println("XML requested");
 		String location = getLocation(req, "location");
 		if(location == null){
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		File requested = new File("/WebContent" + location);
+		File requested = new File("./WebContent" + location);
+
+		if(!requested.exists()){
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.getWriter().write("File does not exist");
+			return;
+		}
+		
+		//FileInputStream fIS = new FileInputStream(requested);
 		resp.setContentType("application/xml");
 		resp.setCharacterEncoding("UTF-8");
-		//resp.getWriter().write(s);
-	}
-	
+		resp.setContentLength((int) requested.length());
+		
+		BufferedInputStream input = null;
+       BufferedOutputStream output = null;
+       
+		try {
+           // Open streams.
+           input = new BufferedInputStream(new FileInputStream(requested), DEFAULT_BUFFER_SIZE);
+           output = new BufferedOutputStream(resp.getOutputStream(), DEFAULT_BUFFER_SIZE);
+
+           // Write file contents to response.
+           byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+           int length;
+           while ((length = input.read(buffer)) > 0) {
+               output.write(buffer, 0, length);
+           }
+       } finally {
+           output.close();
+           input.close();
+       }
+}
 	private String getLocation(HttpServletRequest req, String name) {
 		String val = req.getParameter(name);
 		if (val == null) {
@@ -57,5 +77,5 @@ public class getDataAjax extends HttpServlet {
 			return null;
 		}
 	}
-	 
+
 }
